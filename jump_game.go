5d4c3330074d0286db94d077ad36e36a7c8e57ec
sigurn <1250476871@qq.com>
2022-08-01 08:21:07 +0800

    Changes

---
+++ jump_game.go
@@ -0,0 +1,27 @@
+package leetcode
+
+func jump(nums []int) int {
+	m := 0
+	step := 0
+	end := 0
+	for i := 0; i < len(nums)-1; i++ {
+
+		m = max(i+nums[i], m)
+
+		if i == end {
+			end = m
+			step++
+		}
+		if m > len(nums)-1 {
+			step++
+			break
+		}
+	}
+	return step
+}
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
---
+++ level-sum.go
@@ -0,0 +1,34 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxLevelSum(root *TreeNode) int {
+	queue := []*TreeNode{root}
+	max := -1000000
+	ans := 1
+	level := 1
+	for len(queue) > 0 {
+		sum := 0
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			sum += queue[i].Val
+			if queue[i].Left != nil {
+				queue = append(queue, queue[i].Left)
+			}
+			if queue[i].Right != nil {
+				queue = append(queue, queue[i].Right)
+			}
+		}
+		if sum > max {
+			max = sum
+			ans = level
+		}
+		queue = queue[size:]
+		level++
+	}
+	return level
+}
